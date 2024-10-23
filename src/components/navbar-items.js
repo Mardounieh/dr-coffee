24d2aa5ec578787c58ce6ba4e064ea5aed0b6d47
Amir mahdi <146704617+Mardounieh@users.noreply.github.com>
2024-10-23 21:11:31 +0330

    fix:header navigation bar

--- src/components/navbar-items.js
+++ src/components/navbar-items.js
@@ -1,17 +1,104 @@
 export const NavbarItems = [
   {
     title: "Product Series",
+    left: "left-[30%]",
+    children: [
+      {
+        title: "Coffee shop",
+      },
+      {
+        title: "CVS & Ho.Re.Ca",
+      },
+      {
+        title: "OCS",
+      },
+      {
+        title: "Home",
+      },
+      {
+        title: "Accessories",
+      },
+      {
+        title: "Shop now",
+      },
+    ],
   },
   {
     title: "Service & Support",
+    left: "left-[38%]",
+    children: [
+      {
+        title: "Our principles",
+      },
+      {
+        title: "Contact specialist",
+      },
+      {
+        title: "FAQs",
+      },
+      {
+        title: "Watch tutorial",
+      },
+      {
+        title: "Download manual",
+      },
+    ],
   },
   {
     title: "About Us",
+    left: "left-[46%]",
+    children: [
+      {
+        title: "Company",
+      },
+      {
+        title: "Our Business",
+      },
+      {
+        title: "Our technology",
+      },
+      {
+        title: "Our quality",
+      },
+      {
+        title: "Contact Us",
+      },
+    ],
   },
   {
     title: "Brand Center",
+    left: "left-[53%]",
+    children: [
+      {
+        title: "News",
+      },
+      {
+        title: "Videos",
+      },
+      {
+        title: "Pictures",
+      },
+    ],
   },
   {
     title: "Coffee Academy",
+    left: "left-[61%]",
+    children: [
+      {
+        title: "Introduction",
+      },
+      {
+        title: "Coffee experts",
+      },
+      {
+        title: "Course center",
+      },
+      {
+        title: "Photos",
+      },
+      {
+        title: "Consult now",
+      },
+    ],
   },
 ];
--- src/home/header/nav-bar/navbar.jsx
+++ src/home/header/nav-bar/navbar.jsx
@@ -1,18 +1,34 @@
 import { IoIosArrowDown } from "react-icons/io";
 import { NavbarItems } from "../../../components/navbar-items";
+import { useState } from "react";
 
 export default function Navbar() {
   return (
     <ul className="flex gap-6">
       {NavbarItems.map((item, index) => {
         return (
-          <li
-            key={index}
-            className="flex items-center gap-1 duration-150 text-zinc-100 hover:text-zinc-400 text-sm cursor-pointer"
-          >
-            {item.title} 
-            <IoIosArrowDown className="mt-1"/>{" "}
-          </li>
+          <>
+            <li
+              key={index}
+              className="group/item inner-hover duration-150 text-zinc-100 hover:text-zinc-400 text-sm cursor-pointer"
+            >
+              <a href="/" className="flex items-center gap-1">
+                {item.title}
+                <IoIosArrowDown className="mt-1" />{" "}
+              </a>
+            </li>
+            <div
+              className={`w-1/12 bg-zinc-100 p-2 rounded text-center absolute flex flex-col gap-2 text-[13px] font-light shadow-md ${item.left} top-12`}
+            >
+              {item.children.map((child, i) => {
+                return (
+                  <a href="/" key={i} className="duration-150 hover:text-red-600">
+                    {child.title}
+                  </a>
+                );
+              })}
+            </div>
+          </>
         );
       })}
     </ul>
