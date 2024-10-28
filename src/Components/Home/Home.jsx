25b5811c36033e3f1921fc34b9f52e31ddbc6bea
Amir mahdi <146704617+Mardounieh@users.noreply.github.com>
2024-10-28 20:14:26 +0330

    completes main tag

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,15 +9,20 @@ import product1 from "../../assets/picture/product1-Bip.png";
 import product2 from "../../assets/picture/product2-Bip.png";
 import product3 from "../../assets/picture/product3-Bip.png";
 import product4 from "../../assets/picture/product4-Bip.png";
-import technical1 from "../../assets/picture/technical1.jpg"
-import technical2 from "../../assets/picture/technical2.jpg"
-import technical3 from "../../assets/picture/technical3.jpg"
-import technical4 from "../../assets/picture/technical4.jpg"
+import technical1 from "../../assets/picture/technical1.jpg";
+import technical2 from "../../assets/picture/technical2.jpg";
+import technical3 from "../../assets/picture/technical3.jpg";
+import technical4 from "../../assets/picture/technical4.jpg";
+import news1 from "../../assets/picture/news1.jpg"
+import news2 from "../../assets/picture/news2.jpg"
+
+
+// Icon
 import { IoArrowForward } from "react-icons/io5";
 
 export default function Home() {
   return (
-    <main className="w-11/12 mt-10">
+    <main className="w-11/12 mt-10 flex flex-col gap-10">
       <article className="flex flex-col gap-10">
         <div className="flex flex-col items-center gap-2">
           <h2 className="text-2xl md:text-3xl font-bold">EXPLORE PRODUCTS</h2>
@@ -55,29 +60,73 @@ export default function Home() {
               Customer-oriented, technology-driven
             </p>
           </div>
-          <button className="bg-black hover:bg-red-600 duration-200 flex items-center gap-1 px-4 py-1.5  rounded-full text-white text-sm font-light">Explore <IoArrowForward className="mt-1"/></button>
+          <button className="bg-black hover:bg-red-600 duration-200 flex items-center gap-1 px-4 py-1.5  rounded-full text-white text-sm font-light">
+            Explore <IoArrowForward className="mt-1" />
+          </button>
           <div className="w-10/12 flex gap-5">
             <div>
               <div className="overflow-hidden rounded-xl">
-                <img className="duration-1000 hover:scale-105" src={technical1} alt="A barista" />
+                <img
+                  className="duration-1000 hover:scale-105"
+                  src={technical1}
+                  alt="A barista"
+                />
               </div>
             </div>
             <div className="flex flex-col gap-2">
               <div className="flex gap-2">
                 <div className="overflow-hidden rounded-xl">
-                  <img className="duration-1000 hover:scale-105" src={technical2} alt="A man enjoying coffee" />
+                  <img
+                    className="duration-1000 hover:scale-105"
+                    src={technical2}
+                    alt="A man enjoying coffee"
+                  />
                 </div>
                 <div className="overflow-hidden rounded-xl">
-                  <img className="duration-1000 hover:scale-105" src={technical3} alt="two people having fun" />
+                  <img
+                    className="duration-1000 hover:scale-105"
+                    src={technical3}
+                    alt="two people having fun"
+                  />
                 </div>
               </div>
               <div className="overflow-hidden rounded-xl">
-                <img className="duration-1000 hover:scale-105" src={technical4} alt="A business meeting" />
+                <img
+                  className="duration-1000 hover:scale-105"
+                  src={technical4}
+                  alt="A business meeting"
+                />
               </div>
             </div>
           </div>
         </div>
       </article>
+      <div className="flex gap-5">
+        <div className="relative min-h-96 group bg-cover bg-center flex flex-col items-center pt-16 gap-5 text-white overflow-hidden rounded-lg aspect-[8/3] text-center">
+          <img
+            className="group-hover:scale-105 duration-1000 absolute inset-0 w-full h-full -z-10 object-cover"
+            src={news1}
+            alt="Card Background"
+          />
+          <h2 className="text-2xl lg:text-3xl font-bold">Product Distribution</h2>
+          <p className="text-sm font-light w-5/6">Dr. Coffee's product and services now cover 100+ countries and regions</p>
+          <button className="flex items-center gap-1 bg-white py-1.5 px-4 rounded-full text-black text-sm font-light hover:text-white hover:bg-red-600 duration-200">
+            Explore <IoArrowForward className="mt-1" />
+          </button>
+        </div>
+        <div className="relative min-h-80 group bg-cover bg-center flex flex-col items-center pt-16 gap-5 text-white overflow-hidden rounded-lg aspect-[8/3] text-center">
+          <img
+            className="group-hover:scale-105 duration-1000 absolute inset-0 w-full h-full -z-10 object-cover"
+            src={news2}
+            alt="Card Background"
+          />
+          <h2 className="text-2xl lg:text-3xl font-bold">Latest News</h2>
+          <p className="text-sm font-light w-5/6">All the latest updates about Dr. Coffee</p>
+          <button className="flex items-center gap-1 bg-white py-1.5 px-4 rounded-full text-black text-sm font-light hover:text-white hover:bg-red-600 duration-200">
+            Explore <IoArrowForward className="mt-1" />
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
